6a563f723218612465b1d22e8e17268f4c7da05e
johnnykinny <144209969+johnnykinny@users.noreply.github.com>
2023-09-06 20:31:49 +0800

    Add files via upload

---
+++ week1_4109042085/week1_4109042085.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+height = float(input("請輸入身高(公尺):"))
+weight = float(input("請輸入體重(公斤):"))
+
+bmi =weight/(height**2)
+print(f"您的bmi是:{bmi:.2f}")
