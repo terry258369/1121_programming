621425794c546ae6a263e6ab8714ba8c9b1c8362
kristin516 <144210761+kristin516@users.noreply.github.com>
2023-09-06 16:02:25 +0800

    Add files via upload

---
+++ week1_4112029009.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x=float(input("請輸入你的身高(公尺):"))
+y=float(input("請輸入你的體重(公斤):"))
+BMI=y/(x**2)
+print(f"您的BMI是:{BMI:2f}")
